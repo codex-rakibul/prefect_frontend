020cd6b98eb30cc03234d466c156a3dbc13d9e2f
rakibul667s <rakibulislam667s@gmail.com>
2023-04-02 14:23:49 +0600

    logo add

--- components/hero/index.js
+++ components/hero/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Navbar from "../navbar";
 
 export default function HeroSection() {
   return (
-    <div className="relative">
+    <div className="relative ">
       <div className="gradient-1 absolute object-cover w-full h-full"></div>
-      <div className="gradient-1 absolute object-cover w-full h-full"></div>
+      <div className="gradient-2 absolute object-cover w-full h-full"></div>
+      <Navbar/>
       <section className="text-white body-font">
         <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
           <div className="text-center lg:w-2/3 w-full">
